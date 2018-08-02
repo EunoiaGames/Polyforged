69b055115618bddeddcd07b3f2720b97d2d88d0d
zach <eunoiagamess@gmail.com>
2018-08-02 01:07:34 -0500

    Added a display for windows os and an opengl context

--- Eunoia Games/Src/Main.cpp
+++ Eunoia Games/Src/Main.cpp
@@ -5,7 +5,7 @@ int main()
 	using namespace Eunoia;
 	using namespace Rendering;
 
-	Display* pDisplay = Display::CreateDisplay("Poly Forged", 1280, 720, GraphicsAPI::GRAPHICS_API_OPENGL);
+	Display* pDisplay = Display::CreateDisplay("Polyforged", 1280, 720, GraphicsAPI::GRAPHICS_API_OPENGL);
 
 	RenderContext* pContext = RenderContext::GetRenderContext();
 	pContext->SetDisplayAsRenderTarget();
